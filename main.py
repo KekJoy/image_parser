d318e8e7db36777bff17cf40d5542e6c45bd7ca8
KekJoy <valiullin.anton2003@yandex.ru>
2023-10-17 22:14:12 +0500

    init

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from icrawler.builtin import GoogleImageCrawler
 import pandas as pd
 import os
-import asyncio
 
 # Укажите путь к XLS файлу, где содержится список ключевых слов
 xls_file_path = '/home/superiorkilljoy/Projects/image_parser/names.xlsx'
@@ -12,8 +11,11 @@ df = pd.read_excel(xls_file_path)
 # Задайте путь для сохранения изображений
 base_storage_path = '/home/superiorkilljoy/Projects/image_parser/images'
 
-async def crawl_images(name, quantity):
-    storage_path = os.path.join(base_storage_path, name)
+# Создадим отдельную папку для каждого имени
+for index, row in df.iterrows():
+    name = row['Keyword']  # Предполагается, что в XLS файле есть столбец с названием "Keyword"
+    quantity = 100  # Количество загружаемых фотографий
+    storage_path = os.path.join(base_storage_path, name)  # Путь для сохранения фотографий для текущего имени
 
     # Создать папку, если она не существует
     if not os.path.exists(storage_path):
@@ -22,16 +24,4 @@ async def crawl_images(name, quantity):
     google_crawler = GoogleImageCrawler(storage={'root_dir': storage_path})
     google_crawler.crawl(keyword=name, max_num=quantity)
 
-async def main():
-    tasks = []
-
-    for index, row in df.iterrows():
-        name = row['Keyword']
-        quantity = 100
-        tasks.append(crawl_images(name, quantity))
-
-    await asyncio.gather(*tasks)
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    # После загрузки фотографий можно сделать что-то с результатами, например, перейти к следующей строке в XLS файле.
